8167f527094b04becd29dfc68bbd8dc9577ebb6a
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-03-05 18:28:45 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -22,7 +22,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "Shivraj" => true
 
 let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
 
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//********************** Operattions ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " shivraj"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3+4) * 6 %3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
