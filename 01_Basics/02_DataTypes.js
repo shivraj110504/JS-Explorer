92824fd5fbe361902c1c990a819eb6e4cce4f701
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-03-05 17:46:40 +0000

    Datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict";   //enables all JS code as newer version
+
+//alert(3+3); //we are using node js, not browser so not working
+
+console.log(3+3)//make sure the code readibility is high.
+console.log("Shivraj Taware")
+
+let name = "shivraj"
+let age = 21
+let isLoggedIn = false;
+
+// number => 2 to power 53;
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
