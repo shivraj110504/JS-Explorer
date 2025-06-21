69afd2d68f21d90164eda2dd95c44587af96887a
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-06-21 10:07:09 +0000

    Conversion of DataTypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "Shivraj";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valInNumber = Number(score);
+//console.log(typeof valInNumber);
+//console.log(valInNumber);
+
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+// true => 1; false => 0;
+
+let isLoggedIn = "shivraj";
+let booleanLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanLoggedIn)
+
+// 1 => true; o => false;
+//"" => false
+// "Shivraj" => true
+
+let someNumber = 333;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
