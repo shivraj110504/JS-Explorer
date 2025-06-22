2bc601eb2e712c9705909468977dff25cca56c98
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-06-22 04:53:08 +0000

    comparison operations studies

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 == 1)
+// console.log(2 <= 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 > 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1) => convert string to the number
+
+
+// try to avoid following conditions, comparison operations
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === [strict check]
+
+console.log("2" === 2);
