c99fa4db289444f27a135a3de109c3337fc9054d
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-06-23 13:43:01 +0000

    done with math and num

--- 01_Basics/06_num_and_math.js
+++ 01_Basics/06_num_and_math.js
@@ -17,9 +17,18 @@ const hundreds = 1000000
 
 //****************** MATH **************************//
 
-console.log(Math)
-console.log(Math.abs(-4))
-console.log(Math.round(4.3))
-console.log(Math.ceil(4.1))
-console.log(Math.floor(4.9))
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3,  3, 6,1 ,26))
+// console.log(Math.max(4, 12, 5, 1, 0))
 
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random()* 10)+1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min +1))+ min)
