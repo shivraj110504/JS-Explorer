b53d6da401671a646b692023173774a6f3fe2985
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-06-23 08:23:15 +0000

    done with some more numbers

--- 01_Basics/06_num_and_math.js
+++ 01_Basics/06_num_and_math.js
@@ -1,14 +1,25 @@
 const score = 400
-console.log(score)
+// console.log(score)
 
 const balance = new Number(100)
-console.log(balance)
+// console.log(balance)
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
 
 const otherNumber = 128.2866
-console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toPrecision(4))
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN')); //=> gives commans according to indian standards (bcz, en-IN id added)
+
+
+
+//****************** MATH **************************//
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(4.9))
+
