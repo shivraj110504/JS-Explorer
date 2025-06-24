519da07504b6d1424dd5a0737a0f61e0810fdb97
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-06-24 14:25:37 +0000

    updated dataTypes

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -19,6 +19,31 @@ let isLoggedIn = false;
 //undefined => [value not assigned yet]
 //symbol => unique
 
+
+
+// 'typeof null' is 'object'
+// console.log(typeof null) 
+
+//Ex.Undefined : when variable is decalred and if no value assigned to it, then it is undefined
+// let myVariable;
+// console.log(myVariable); //undefined
+// console.log(typeof myVariable); //undefined
+
+// Null :
+// null expresses a lack of identification, indicating that a variable points to no object
+// typeof null; // "object" (not "null" for legacy reasons)
+// typeof undefined; // "undefined"
+// null === undefined; // false
+// null == undefined; // true
+// null === null; // true
+// null == null; // true
+// !null; // true
+// Number.isNaN(1 + null); // false
+// Number.isNaN(1 + undefined); // true
+
+
+
+
 //object
 
 console.log(typeof undefined)
