ede8b1e0a53065a5c2a3b382f7c3756eaf8c59dc
Shivraj S. Taware <tawareshivraj11@gmail.com>
2025-07-26 12:32:11 +0000

    done with unlimited color project

---
+++ 08_Projects/06_unlimitedColors/scripts.js
@@ -0,0 +1,31 @@
+
+
+// ************** function to generate random colors *********************************
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color= '#';
+    for(let i = 0 ; i < 6 ; i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    // console.log(color);
+    return color;
+}
+
+let intervelId;
+const startChangingColor = function(){
+    if(!intervelId){
+         intervelId = setInterval(changeColor, 1000);
+    }
+
+    function changeColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+    
+const stopChangingColor = function(){
+    clearInterval(intervelId);
+    intervelId = null
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
